55f0439864ba3a74685d9331f084fc82f8a7e722
mmagny <mikemagny@gmail.com>
2019-03-01 22:06:01 -0500

    End of module "Classes and Objects"
    Add Feature to show Average, Highest and Lowest grade

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                /*if(grade > stats.HighestGrade)
+                {
+                    stats.HighestGrade = grade;
+                }
+
+                if(grade < stats.LowestGrade)
+                {
+                    stats.LowestGrade = grade;
+                }*/
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade; 
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<float> grades;
+    }
+}

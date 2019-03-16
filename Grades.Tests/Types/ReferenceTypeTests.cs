cac5cab401790d23aa1001a3edbab2013ab5619a
mmagny <mikemagny@gmail.com>
2019-03-15 20:41:24 -0400

    backup

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -8,16 +8,75 @@ using System.Threading.Tasks;
 namespace Grades.Tests.Types
 {
     [TestClass]
-    public class ReferenceTypeTests
+    public class TypeTests
     {
         [TestMethod]
-        public void VariableHoldAReference()
+        public void ValueTypePassByValue()
         {
-            GradeBook g1 = new GradeBook();
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void RefrenceTypesByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            //book = new GradeBook();
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
+        public void StringCamparisons()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+
+            bool result = string.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void IntVariableHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariableHoldAReference()
+        {
+            /*GradeBook g1 = new GradeBook();
             GradeBook g2 = g1;
 
             g1.Name = "Scott's grade book";
-            Assert.AreEqual(g1.Name, g2.Name);
+            Assert.AreEqual(g1.Name, g2.Name);*/
+
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1 = new GradeBook();
+            g1.Name = "Scott's grade book";
+            Assert.AreNotEqual(g1.Name, g2.Name);
         }
 
     }

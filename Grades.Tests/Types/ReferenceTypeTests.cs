33310780019be9cff0c72ed2a0b538330db5b813
mmagny <mikemagny@gmail.com>
2019-03-18 21:09:53 -0400

    backup

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -11,6 +11,43 @@ namespace Grades.Tests.Types
     public class TypeTests
     {
         [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            //grades = new float[5];
+            //Try arrays of class ??
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+
+        }
+
+        [TestMethod]
         public void ValueTypePassByValue()
         {
             int x = 46;
